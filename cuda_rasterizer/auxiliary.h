f9449374216754718ac6ab74f69daae1d78c080f
PaxosPro <144289259+PaxosPro@users.noreply.github.com>
2024-02-24 00:36:41 +0100

    Update auxiliary.h

--- cuda_rasterizer/auxiliary.h
+++ cuda_rasterizer/auxiliary.h
@@ -151,10 +151,7 @@ __forceinline__ __device__ bool in_frustum(int idx,
 	float p_w = 1.0f / (p_hom.w + 0.0000001f);
 	float3 p_proj = { p_hom.x * p_w, p_hom.y * p_w, p_hom.z * p_w };
 	p_view = transformPoint4x3(p_orig, viewmatrix);
-	printf("p_orig: ");
-    print_float3(p_orig);
-    printf("p_proj: ");
-    print_float3(p_proj);
+
 	if (p_view.z <= 0.2f)// || ((p_proj.x < -1.3 || p_proj.x > 1.3 || p_proj.y < -1.3 || p_proj.y > 1.3)))
 	{
 		if (prefiltered)
